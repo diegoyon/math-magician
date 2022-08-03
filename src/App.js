960b62a7a931058765d06e41fded2aacc867d186
Diego Yon <diegoyon@outlook.com>
2022-08-03 09:34:08 -0600

    Refactor classes to functions using hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
