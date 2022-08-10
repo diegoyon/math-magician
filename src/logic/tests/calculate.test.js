ad65e6ab22eaaaebf9ed6e63fc9b97cca50b906b
AbrahaKahsay <abrahak81@gmail.com>
2022-08-11 00:59:21 +0300

    Add tests to calculate.js

--- src/logic/tests/calculate.test.js
+++ src/logic/tests/calculate.test.js
@@ -21,4 +21,51 @@ describe('calculate tests',() => {
   test('if there is an operation an next = 0', () => {
     expect(calculate({ next: '0', operation: '+'}, '3')).toEqual({ next: '3', operation: '+'});
   })
+  test('if there is no operation', ()=>{
+    expect(calculate({next: '4'}, '2')).toEqual({next:'42', total: null})
+  })
+  test('if there is empty object', ()=>{
+    expect(calculate({}, '9')).toEqual({next:'9', total: null})
+  })
+
+  test("if the button is '.'", ()=>{
+    expect(calculate({next: '5.7'}, '.')).toEqual({next:'5.7'})
+  })
+
+  test("if the button is '.' and number is integer", ()=>{
+    expect(calculate({next: '5'}, '.')).toEqual({next:'5.'})
+  })
+  test("if the button is '.' and with operation", ()=>{
+    expect(calculate({operation: '+'}, '.')).toEqual({operation:'+', next:'0.'})
+  })
+  test("if the button is '.' and object has total", ()=>{
+    expect(calculate({total: '5.8'}, '.')).toEqual({})
+  })
+  test("if the button is '.' and number has total", ()=>{
+    expect(calculate({total: '4'}, '.')).toEqual({total:'4', next:'4.'})
+  })
+  test("if the button is '.' and number 0.", ()=>{
+    expect(calculate({}, '.')).toEqual({next:'0.'})
+  })
+
+  test("if the button is '=' with objects", ()=>{
+    expect(calculate({next: '5', total: '3', operation: '+'}, '=')).toEqual({total:'8', next:null, operation: null})
+  })
+  test("if the button is '=' with empty object ", ()=>{
+    expect(calculate({}, '=')).toEqual({})
+  })
+
+  test("if the button is '+/-' with next object ", ()=>{
+    expect(calculate({next: '2'}, '+/-')).toEqual({next:'-2'})
+  })
+  test("if the button is '+/-' with total object ", ()=>{
+    expect(calculate({total: '2'}, '+/-')).toEqual({total:'-2'})
+  })
+  test("if the button is '+/-' with empty object ", ()=>{
+    expect(calculate({}, '+/-')).toEqual({})
+  })
+
+  test("User pressed an operation after pressing '='", ()=>{
+    expect(calculate({total: '6'}, '-')).toEqual({total:'6', operation:'-'})
+  })
 })
